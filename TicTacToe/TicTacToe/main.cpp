b3e5fb0e62f03d5753675b0a208f50d7e40963df
20063914 <20063914@it102imac06.wit.ie>
2017-03-01 09:56:36 +0000

    Initial setup of TicTacToe

---
+++ TicTacToe/TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by 20063914 on 01/03/2017.
+//  Copyright © 2017 20063914. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
