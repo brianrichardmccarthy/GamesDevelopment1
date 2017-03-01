c50d4b242674953c8182f5bb348591b5ace2da4e
20063914 <20063914@it102imac06.wit.ie>
2017-03-01 10:08:45 +0000

    Add function prototypes

--- TicTacToe/TicTacToe/main.cpp
+++ TicTacToe/TicTacToe/main.cpp
@@ -11,12 +11,30 @@
 #include <vector>
 #include <algorithm>
 
+using std::string;
+using std::cout;
+using std::vector;
+using std::cin;
+
 // global constants
 const char X = 'X';
 const char O = 'O';
 const char EMPTY = ' ';
 const char TIE = 'T';
 const char NO_ONE = 'N';
+
+//function prototypes
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char>& board);
+char winner(const vector<char>& board);
+bool isLegal(const vector<char>& board, int move);
+int humanMove(const vector<char>& board, char human);
+int computerMove(vector<char> board, char computer);
+void announceWinner(char winner, char computer, char human);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
